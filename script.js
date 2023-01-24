a1604672b1a707d9b5edf963cb2a7ab737d49779
aingianni92 <ingianni.anthony@gmail.com>
2023-01-24 17:36:12 -0500

    fixed visibility function

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-// const textLine = document.querySelector('.text-line')
+const textLine = document.querySelector('.text-line')
 
-// console.log(textLine)
+console.log(textLine)
 
-// const makeVisible = () => {
-//   if (textLine.style.visibility === 'hidden') {
-//     textLine.style.visibility = 'visible'
-//   } else {
-//     textLine.style.visibility = 'hidden'
-//   }
-// }
+const makeVisible = () => {
+  if (textLine.style.visibility === 'hidden') {
+    textLine.style.visibility = 'visible'
+  } else {
+    textLine.style.visibility = 'hidden'
+  }
+}
 
-// setInterval(makeVisible, 1000)
+setInterval(makeVisible, 1000)
