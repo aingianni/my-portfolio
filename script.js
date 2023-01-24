a2964d805e544681f2a9aeaea303e6b9843f625f
aingianni92 <ingianni.anthony@gmail.com>
2023-01-24 17:26:48 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const textLine = document.querySelector('.text-line')
+
+console.log(textLine)
+
+const makeVisible = () => {
+  if (textLine.style.visibility === 'hidden') {
+    textLine.style.visibility = 'visible'
+  } else {
+    textLine.style.visibility = 'hidden'
+  }
+}
+
+setInterval(makeVisible, 1000)
